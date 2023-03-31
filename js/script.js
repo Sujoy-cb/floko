0620c7bf3d89d16140a48f55eab02db985e28e3e
Sujoy chakraborty <sujoyc070@gmail.com>
2023-03-31 22:31:42 +0600

    floko running project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// slider//
+$('.slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+  });
